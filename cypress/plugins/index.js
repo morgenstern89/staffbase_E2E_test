7c9fcaa52a1a1d2ed0f86c8745c9830532130cf2
morgenstern89 <morgenstern89>
2022-09-09 00:58:14 +0200

    changes made

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,3 +3,8 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 }
+
+const arr = [];
+arr[0].forEach(element => {
+  console.log(element);
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -20,8 +20,23 @@ import './commands'
 // require('./commands')
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // we expect a 3rd party library error with message 'list not defined'
+  // and don't want to fail the test so we return false
+  if (err.message.includes('TypeError: Cannot read properties of undefined (reading 'forEach')')) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
+
+// patch Cypress top.onerror
+Object.defineProperty(top, 'onerror', {
+  value: window.onerror,
 })
 
