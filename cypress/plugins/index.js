49146be8e6a56d2a5dcf7080733ee71842975142
morgentsern89 <sar.yoon89@gmail.com>
2022-09-14 00:10:27 +0200

    changes made

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,30 @@
-const cucumber = require('cypress-cucumber-preprocessor').default
+//import './commands'
+
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = (on, config) => {
-  on('file:preprocessor', cucumber())
+
+    on('file:preprocessor', cucumber())
 }
+
+//module.exports = (on, config) => {
+//  const options = {
+//    ...browserify.defaultOptions,
+//    typescript: require.resolve('typescript'),
+//  };
+//
+//  options.browserifyOptions.plugin.unshift([
+//    'tsify',
+//    { project: 'cypress/tsconfig.json' },
+//  ]);
+//
+//  on('file:preprocessor', browserify());
+//  on('file:preprocessor', cucumber(options));
+//  on('before:browser:launch', (browser, launchOptions) => {
+//    if (browser.name === 'chrome') {
+//      launchOptions.args.push('--disable-dev-shm-usage');
+//    }
+//    return launchOptions;
+//  });
+//};
 
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,10 +19,10 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-})
-
+Cypress.on('uncaught:exception', (err) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  console.log('Cypress detected uncaught exception: ', err);
+  return false;
+});
 
